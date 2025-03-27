83163095aa8c1a654a0974041f828c14900e369c
DamienE <damien.entwistle@red-gate.com>
2025-03-27 10:41:18 -0700

    New Migration Scripts 003

---
+++ migrations/V003__MigrationScript.sql
@@ -0,0 +1,33 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [Customers].[Customer]'
+GO
+ALTER TABLE [Customers].[Customer] ADD
+[X] [nchar] (10) NULL
+GO
+PRINT N'Refreshing [Customers].[CustomerFeedbackSummary]'
+GO
+EXEC sp_refreshview N'[Customers].[CustomerFeedbackSummary]'
+GO
+PRINT N'Refreshing [Sales].[CustomerOrdersView]'
+GO
+EXEC sp_refreshview N'[Sales].[CustomerOrdersView]'
+GO
+PRINT N'Creating [dbo].[AddressBook]'
+GO
+CREATE TABLE [dbo].[AddressBook]
+(
+[AddressBook] [nchar] (10) NULL
+)
+GO
+PRINT N'Creating [dbo].[GoldMembership]'
+GO
+CREATE TABLE [dbo].[GoldMembership]
+(
+[FistName] [nvarchar] (50) NULL,
+[LastName] [nvarchar] (50) NULL
+)
+GO
+
